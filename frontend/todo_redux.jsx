6c2021377eeffd5f38e32df3d3098faa86376c40
Brandon Tat <brandontat7@gmail.com>
2017-08-01 14:30:15 -0700

    add reducer actions

--- frontend/todo_redux.jsx
+++ frontend/todo_redux.jsx
@@ -1,6 +1,18 @@
 import React from 'react';
+import configureStore from './store/store';
+
+//REMOVE AFTER TESTING
+import {receiveTodos, receiveTodo} from './actions/todo_actions';
 
 document.addEventListener('DOMContentLoaded', () => {
+  let store = configureStore;
+
+  // REMOVE AFTER TESTING
+  window.store = store;
+  window.receiveTodos = receiveTodos;
+  window.receiveTodo = receiveTodo;
+  //
+
   const root = document.getElementById('content');
   root.innerHTML = "Todo App";
 });
